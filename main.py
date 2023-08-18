aecbf272c770cc54e668985bd19087d8d872378a
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-08-18 12:56:05 -0500

    Add url endpoint for yearly and all data

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[['STAID', 'STANAME                                 ']]
 
+
 @app.route("/")
 def home():
 	return render_template("home.html", data=stations.to_html())
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -19,6 +21,24 @@ def about(station, date):
 	return {"station": station,
 			"date": date,
 			"temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+	filename = "data_small/TG_STAID" + station.zfill(6) + ".txt"
+	df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+	result = df.to_dict(orient="records")
+	return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+	filename = "data_small/TG_STAID" + station.zfill(6) + ".txt"
+	df = pd.read_csv(filename, skiprows=20)
+	df["    DATE"] = df["    DATE"].astype(str)
+	result = df[df["    DATE"].str.startswith(year)].to_dict(orient="records")
+	return result
+
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5001)
